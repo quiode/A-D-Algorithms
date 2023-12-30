4caf0966fbc55201abb8e0914456ad0331fcc8c5
Dominik Schwaiger <mail@dominik-schwaiger.ch>
2023-12-30 13:58:53 +0100

    ShortestPath

--- src/Graph.java
+++ src/Graph.java
@@ -1,8 +1,18 @@
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class Graph {
+	/**
+	 * Amount of nodes
+	 */
+	public final int n;
+	/*
+	 * Amount of edges
+	 */
+	public final int m;
+
 	/**
 	 * adj[i][j] gets the j-th neighbour of node i.
 	 */
@@ -15,20 +25,27 @@ public class Graph {
 	public Graph() {
 		adj = new ArrayList<>();
 		weights = new ArrayList<>();
+		n = 0;
+		m = 0;
 	}
 
 	public Graph(int[][] adj, int[][] weights) {
 		this.adj = new ArrayList<>(adj.length);
 		this.weights = new ArrayList<>(adj.length);
 
+		int edges = 0;
 		for (int i = 0; i < adj.length; i++) {
 			this.adj.set(0, new ArrayList<>(adj[i].length));
 			this.weights.set(0, new ArrayList<>(adj[i].length));
 			for (int j = 0; j < adj[i].length; j++) {
+				edges++;
 				this.adj.get(i).add(adj[i][j]);
 				this.weights.get(i).add(weights[i][j]);
 			}
 		}
+
+		n = this.adj.size();
+		m = edges;
 	}
 
 	public Graph(Graph g) {
@@ -39,6 +56,27 @@ public class Graph {
 			adj.add(new ArrayList<>(g.adj.get(i)));
 			weights.add(new ArrayList<>(g.weights.get(i)));
 		}
+
+		n = g.n;
+		m = g.m;
+	}
+
+	public Graph(List<List<Integer>> adj, List<List<Integer>> weights) {
+		this.adj = adj.stream().map(list -> list.stream().collect(Collectors.toList())).collect(Collectors.toList());
+		this.weights = weights.stream().map(list -> list.stream().collect(Collectors.toList()))
+				.collect(Collectors.toList());
+
+		n = this.adj.size();
+		m = this.weights.stream().map(Collection::size).reduce(0, (a, b) -> a + b);
+	}
+
+	public Graph addNode(List<Integer> adj, List<Integer> weights) {
+		List<List<Integer>> newAdj = getAdj();
+		newAdj.add(new ArrayList<>(adj));
+		List<List<Integer>> newWeights = getWeights();
+		newWeights.add(new ArrayList<>(weights));
+
+		return new Graph(newAdj, newWeights);
 	}
 
 	public List<List<Integer>> getAdj() {
---
+++ src/MST.java
@@ -0,0 +1,14 @@
+
+public class MST {
+	public static void Prim(Graph g) {
+
+	}
+
+	public static void Boruvka(Graph g) {
+
+	}
+
+	public static void Kruskal(Graph g) {
+
+	}
+}
