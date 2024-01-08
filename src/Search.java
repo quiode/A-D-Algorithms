512e77540a1aca1b30b1adc43d68149176934199
Dominik Schwaiger <mail@dominik-schwaiger.ch>
2024-01-08 08:34:34 +0100

    search

--- src/Search.java
+++ src/Search.java
@@ -1,9 +1,29 @@
 
 public class Search {
 	public static int linear(int[] A, int x) {
+		for (int i = 0; i < A.length; i++) {
+			if (A[i] == x)
+				return i;
+		}
 
+		return -1;
 	}
 
 	public static int binary(int[] A, int x) {
+		return binary(A, x, 0, A.length - 1);
+	}
+
+	private static int binary(int[] A, int x, int l, int r) {
+		if (l > r)
+			return -1;
+
+		int m = (l + r) / 2;
+		if (A[m] == x) {
+			return m;
+		} else if (A[m] < x) {
+			return binary(A, x, m + 1, r);
+		} else {
+			return binary(A, x, l, m - 1);
+		}
 	}
 }
